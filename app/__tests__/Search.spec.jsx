18d9b0b8c445c020d5e0a80a91abd01d8f714127
Federico Cano <canofedericomartin@gmail.com>
2017-09-19 11:46:24 -0300

    Adding snapshot tests for Search component

---
+++ app/__tests__/Search.spec.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Search from '../components/Search';
+import ShowCard from '../components/ShowCard';
+import preload from '../../data.json';
+
+describe('Search component', () => {
+  const componentWithSearchTerm = (searchTerm = '') =>
+    shallow(
+      <Search shows={preload.shows} searchTerm={searchTerm} handleSearchTermChange={f => f} />
+    );
+
+  const componentWithoutSearchTerm = componentWithSearchTerm();
+
+  it('should renders correctly', () => {
+    expect(componentWithoutSearchTerm).toMatchSnapshot();
+  });
+
+  it('should render correct amount of shows', () => {
+    expect(componentWithoutSearchTerm.find(ShowCard).length).toEqual(preload.shows.length);
+  });
+
+  it('should render correct amount of shows based on searchTerm', () => {
+    expect(componentWithSearchTerm('black').find(ShowCard).length).toEqual(2);
+  });
+});
