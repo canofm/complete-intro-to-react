e5b015e7158fc7e14db4428f24bf62c88a8f2aa4
Federico Cano <canofedericomartin@gmail.com>
2017-09-18 21:33:20 -0300

    Adding BrowserRouter and react-router-dom

--- app/ClientApp.jsx
+++ app/ClientApp.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import Landing from './components/Landing';
+import Search from './components/Search';
 
 const App = () => (
-  <div className="app">
-    <Landing />
-  </div>
+  <BrowserRouter>
+    <div className="app">
+      <Route exact path="/" component={Landing} />
+      <Route path="/search" component={Search} />
+    </div>
+  </BrowserRouter>
 );
 
 render(<App />, document.getElementById('app'));
