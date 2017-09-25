403607a7c02482382fc92cde3b842caa481627b4
Federico Cano <canofedericomartin@gmail.com>
2017-09-25 15:25:51 -0300

    Adding BackButton component

---
+++ app/components/BackButton.jsx
@@ -0,0 +1,12 @@
+// @flow
+
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const BackButton = () => (
+  <h2>
+    <Link to="/search">Back</Link>
+  </h2>
+);
+
+export default BackButton;
