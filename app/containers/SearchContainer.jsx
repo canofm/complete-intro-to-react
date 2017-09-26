459b896880d06ae5092d108c405159a0b7d6311e
Federico Cano <canofedericomartin@gmail.com>
2017-09-26 15:46:37 -0300

    Refactoring SearchContainer as High Order Component

--- app/containers/SearchContainer.jsx
+++ app/containers/SearchContainer.jsx
@@ -1,29 +1,26 @@
 // @flow
 
 import React, { Component } from 'react';
-import Search from '../components/Search';
 
-class SearchContainer extends Component {
-  state = {
-    searchTerm: ''
-  };
-  props: {
-    shows: Array<Show>
-  };
+const SearchContainer = (SearchComponent: Component) =>
+  class extends Component {
+    state = {
+      searchTerm: ''
+    };
 
-  handleSearchTermChange = (event: SyntheticKeyboardEvent & { target: HTMLInputElement }) => {
-    this.setState({ searchTerm: event.target.value });
-  };
+    handleSearchTermChange = (event: SyntheticKeyboardEvent & { target: HTMLInputElement }) => {
+      this.setState({ searchTerm: event.target.value });
+    };
 
-  render() {
-    return (
-      <Search
-        searchTerm={this.state.searchTerm}
-        handleSearchTermChange={this.handleSearchTermChange}
-        shows={this.props.shows}
-      />
-    );
-  }
-}
+    render() {
+      return (
+        <SearchComponent
+          searchTerm={this.state.searchTerm}
+          handleSearchTermChange={this.handleSearchTermChange}
+          {...this.props}
+        />
+      );
+    }
+  };
 
 export default SearchContainer;
