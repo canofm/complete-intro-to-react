1e21ea5d9ecdada751274e6efe4f12f07bcded61
Federico Cano <canofedericomartin@gmail.com>
2017-09-26 15:48:59 -0300

    Refactoring Landing to use SearchInput component

--- app/components/Landing.jsx
+++ app/components/Landing.jsx
@@ -2,13 +2,21 @@
 
 import React from 'react';
 import { Link } from 'react-router-dom';
+import SearchInput from './SearchInput';
 
-const Landing = () => (
+const Landing = (props: { searchTerm?: string, handleSearchTermChange: Function }) => (
   <div className="landing">
     <h1>svideo</h1>
-    <input type="text" placeholder="Search" />
+    <SearchInput
+      searchTerm={props.searchTerm}
+      handleSearchTermChange={props.handleSearchTermChange}
+    />
     <Link to="/search">or Browse All</Link>
   </div>
 );
+
+Landing.defaultProps = {
+  searchTerm: ''
+};
 
 export default Landing;
