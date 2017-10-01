ca504f3bcce85098a702dbc150b686e8da001a4c
Federico Cano <canofedericomartin@gmail.com>
2017-10-01 16:43:55 -0300

    Now when typing on landing and press enter redirect to search

--- app/components/Landing.jsx
+++ app/components/Landing.jsx
@@ -2,36 +2,26 @@
 
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import { connect } from 'react-redux';
+import type { RouterHistory } from 'react-router-dom';
 import SearchInput from './SearchInput';
-import { setSearchTerm } from '../actions';
 
-const mapStateToProps = state => ({ searchTerm: state.searchTerm });
-const mapDispatchToProps = (dispatch: Function) => ({
-  handleSearchTermChange(event) {
-    dispatch(setSearchTerm(event.target.value))
-  }
-})
-
-@connect(mapStateToProps, mapDispatchToProps)
 class Landing extends Component {
-  defaultProps = {
-    searchTerm: ''
-  }
-
   props: {
-    searchTerm: string,
-    handleSearchTermChange: Function
-  }
+    history: RouterHistory
+  };
+
+  goToSearch = event => {
+    event.preventDefault();
+    this.props.history.push('/search');
+  };
 
   render() {
     return (
       <div className="landing">
         <h1>svideo</h1>
-        <SearchInput
-          searchTerm={this.props.searchTerm}
-          handleSearchTermChange={this.props.handleSearchTermChange}
-        />
+        <form onSubmit={this.goToSearch}>
+          <SearchInput />
+        </form>
         <Link to="/search">or Browse All</Link>
       </div>
     );
