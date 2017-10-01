26c488c8cd4d0af54fce53b3ddeb02c36d841b26
Federico Cano <canofedericomartin@gmail.com>
2017-10-01 13:27:42 -0300

    Setting up provider for app

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -3,6 +3,8 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import type { Match } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from '../store';
 import Landing from './Landing';
 import Search from './Search';
 import SearchContainer from '../containers/SearchContainer';
@@ -12,28 +14,30 @@ import preload from '../../data.json';
 
 const App = () => (
   <BrowserRouter>
-    <div className="app">
-      <Switch>
-        <Route exact path="/" component={SearchContainer(Landing)} />
-        <Route
-          path="/search"
-          component={() => {
-            const SearchComponent = SearchContainer(Search);
-            return <SearchComponent shows={preload.shows} />;
-          }}
-        />
-        <Route
-          path="/details/:id"
-          component={(props: { match: Match }) => (
-            <DetailsContainer
-              show={preload.shows.find(show => props.match.params.id === show.imdbID)}
-              {...props}
-            />
-          )}
-        />
-        <Route component={FourOhFour} />
-      </Switch>
-    </div>
+    <Provider store={store}>
+      <div className="app">
+        <Switch>
+          <Route exact path="/" component={SearchContainer(Landing)} />
+          <Route
+            path="/search"
+            component={() => {
+              const SearchComponent = SearchContainer(Search);
+              return <SearchComponent shows={preload.shows} />;
+            }}
+          />
+          <Route
+            path="/details/:id"
+            component={(props: { match: Match }) => (
+              <DetailsContainer
+                show={preload.shows.find(show => props.match.params.id === show.imdbID)}
+                {...props}
+              />
+            )}
+          />
+          <Route component={FourOhFour} />
+        </Switch>
+      </div>
+    </Provider>
   </BrowserRouter>
 );
 
