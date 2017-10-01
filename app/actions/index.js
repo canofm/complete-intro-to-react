d72e6846d8fd510ec9aed3866164e973a49983bf
Federico Cano <canofedericomartin@gmail.com>
2017-10-01 12:35:14 -0300

    Adding setSearchTerm action

---
+++ app/actions/index.js
@@ -0,0 +1,6 @@
+const SET_SEARCH_TERM = 'SET_SEARCH_TERM';
+
+const setSearchTerm = searchTerm => ({ type: SET_SEARCH_TERM, payload: searchTerm });
+
+export { SET_SEARCH_TERM };
+export { setSearchTerm };
