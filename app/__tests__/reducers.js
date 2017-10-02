2b12017b3e4fbde5701a96fb5efb44a91497bbe9
Federico Cano <canofedericomartin@gmail.com>
2017-10-02 13:07:24 -0300

    Testing ADD_API_DATA reducer

--- app/__tests__/reducers.js
+++ app/__tests__/reducers.js
@@ -9,3 +9,28 @@ describe('SET_SEARCH_TERM', () => {
     expect(state).toEqual({ searchTerm: 'game of thrones', apiData: {} });
   });
 });
+
+describe('ADD_API_DATA', () => {
+  it('should apply correct reducer and change state with payload data', () => {
+    const gameOfThrones = {
+      rating: '3.5',
+      title: 'Game of Thrones',
+      year: '2011–',
+      description:
+        'Nine noble families fight for control over the mythical lands of Westeros, while a forgotten race returns after being dormant for thousands of years.',
+      poster: 'got.jpg',
+      imdbID: 'tt0944947',
+      trailer: 'giYeaKsXnsI'
+    };
+
+    const state = reducers(
+      { searchTerm: '', apiData: {} },
+      { type: 'ADD_API_DATA', payload: gameOfThrones }
+    );
+
+    expect(state).toEqual({
+      searchTerm: '',
+      apiData: { tt0944947: gameOfThrones }
+    });
+  });
+});
