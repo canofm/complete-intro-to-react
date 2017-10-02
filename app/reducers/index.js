30a143607c53bbb84fee632f7c6a58becf3d629e
Federico Cano <canofedericomartin@gmail.com>
2017-10-02 00:42:50 -0300

    Refactor reducers using combineReducers

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,18 +1,6 @@
-import { SET_SEARCH_TERM } from '../actions';
+import { combineReducers } from 'redux';
+import searchTerm from './searchTerm';
 
-const DEFAULT_STATE = {
-  searchTerm: ''
-};
-
-const setSearchTerm = (state, action) => Object.assign({}, state, { searchTerm: action.payload });
-
-const rootReducer = (state = DEFAULT_STATE, action) => {
-  switch (action.type) {
-    case SET_SEARCH_TERM:
-      return setSearchTerm(state, action);
-    default:
-      return state;
-  }
-};
+const rootReducer = combineReducers({ searchTerm });
 
 export default rootReducer;
---
+++ app/reducers/searchTerm.js
@@ -0,0 +1,10 @@
+import { SET_SEARCH_TERM } from '../actions';
+
+const searchTerm = (state = '', action) => {
+  if (action.type === SET_SEARCH_TERM) {
+    return action.payload;
+  }
+  return state;
+};
+
+export default searchTerm;
